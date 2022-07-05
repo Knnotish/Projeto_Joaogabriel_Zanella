edff30a55e0a983645ec50ef5c2693705b4f16da
João Gabriel Kondarzewski Zanella <joaogabrielkondazanella@gmail.com>
2022-07-05 14:55:16 -0300

    Criado o método MA3()

--- classe_A.py
+++ classe_A.py
@@ -1,24 +1,26 @@
 class A:
   
-  def __init__(self):
-    self.A1: int
-    self.A2: float
+    def __init__(self):
+        self.A1: int
+        self.A2: float
       
-      
-  def getA1(self):
-    return self.A1
+    def getA1(self):
+        return self.A1
   
-  def setA1(self, novo: int):
-    self.A1 = novo
+    def setA1(self, novo: int):
+        self.A1 = novo
     
-  def getA2(self):
-    return self.A2
+    def getA2(self):
+        return self.A2
   
-  def setA2(self, novo: float):
-    self.A2 = novo
+    def setA2(self, novo: float):
+        self.A2 = novo
     
-  def MA1(self):
-    print("MA1")
+    def MA1(self):
+        print("MA1")
           
-  def MA2(self):
-    print("MA2")
+     def MA2(self):
+        print("MA2")
+
+    def MA3(self):
+        print("Alteração a classe A partir do clone")
