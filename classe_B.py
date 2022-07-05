5bf4328b60f9fdfbdce5e4599b19d255f1e3c794
Knnotish <81074517+Knnotish@users.noreply.github.com>
2022-07-05 14:30:18 -0300

    Update classe_B.py

--- classe_B.py
+++ classe_B.py
@@ -1,1 +1,24 @@
-
+class B:
+  
+  def __init__(self):
+    self.B1: int
+    self.B2: float
+      
+      
+  def getB1(self):
+    return self.B1
+  
+  def setB1(self, novo: int):
+    self.B1 = novo
+    
+  def getB2(self):
+    return self.B2
+  
+  def setB2(self, novo: float):
+    self.B2 = novo
+    
+  def MB1(self):
+    print("MB1")
+          
+  def MB2(self):
+    print("MB2")
