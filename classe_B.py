290de04675ccbdc4223fc38fd971599155782c7b
João Gabriel Kondarzewski Zanella <joaogabrielkondazanella@gmail.com>
2022-07-05 15:30:51 -0300

    Criado o método MB3()

--- classe_B.py
+++ classe_B.py
@@ -1,24 +1,26 @@
 class B:
   
-  def __init__(self):
-    self.B1: int
-    self.B2: float
+     def __init__(self):
+        self.B1: int
+        self.B2: float
       
-      
-  def getB1(self):
-    return self.B1
+    def getB1(self):
+        return self.B1
   
-  def setB1(self, novo: int):
-    self.B1 = novo
+    def setB1(self, novo: int):
+        self.B1 = novo
     
-  def getB2(self):
-    return self.B2
+    def getB2(self):
+        return self.B2
   
-  def setB2(self, novo: float):
-    self.B2 = novo
+    def setB2(self, novo: float):
+        self.B2 = novo
     
-  def MB1(self):
-    print("MB1")
+    def MB1(self):
+        print("MB1")
           
-  def MB2(self):
-    print("MB2")
+    def MB2(self):
+        print("MB2")
+
+    def MB3(self):
+        print("MB3")
