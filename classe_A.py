7ae615ce0c60655b696395598486c47d23dc2e24
Knnotish <81074517+Knnotish@users.noreply.github.com>
2022-07-05 14:29:01 -0300

    Update classe_A.py

--- classe_A.py
+++ classe_A.py
@@ -1,1 +1,24 @@
-
+class A:
+  
+  def __init__(self):
+    self.A1: int
+    self.A2: float
+      
+      
+  def getA1(self):
+    return self.A1
+  
+  def setA1(self, novo: int):
+    self.A1 = novo
+    
+  def getA2(self):
+    return self.A2
+  
+  def setA2(self, novo: float):
+    self.A2 = novo
+    
+  def MA1(self):
+    print("MA1")
+          
+  def MA2(self):
+    print("MA2")
